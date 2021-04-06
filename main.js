2f4dbed4b3272d1d783d775e2e625461357f4a60
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-04-06 15:44:36 +0200

    spawn two types of obstacles works

--- main.js
+++ main.js
@@ -43,10 +43,9 @@ function enterGame(e){
 }
 document.onkeydown = enterGame;
 
-let score, highscore, player, gravity, obstacles, gameSpeed;
+let score, highscore, player, gravity, gameSpeed;
+let obstacles = [];
 let keys = {};
-
-
 
 class Player {
     constructor(x,y,width,height,color){
@@ -57,7 +56,7 @@ class Player {
         this.color = color;
 
         this.directionY = 0; // Jump velocity
-        this.jumpForce = 15;
+        this.jumpForce = 16;
         this.originalHeight = height;
         this.grounded = false;
         this.jumpTimer = 0;
@@ -65,11 +64,18 @@ class Player {
 
     Animate () {
      //Jump
-     if(keys['Space'] || keys['ArrowUp']){
+     if(keys['KeyW'] || keys['ArrowUp']){
         this.Jump();
     } else {
         this.jumpTimer = 0;
     }
+
+    if(keys["KeyS"] || keys["ArrowDown"]){
+        this.height = this.originalHeight / 2;
+    } else {
+        this.height = this.originalHeight
+    }
+
     this.y += this.directionY
      //Gravity
      if(this.y + this.height < canvas.height) {
@@ -89,7 +95,7 @@ class Player {
      if(this.grounded && this.jumpTimer == 0) {
          this.jumpTimer = 1;
          this.directionY =- this.jumpForce;
-     } else if (this.jumpTimer > 0 && this.jumpTimer < 15){
+     } else if (this.jumpTimer > 0 && this.jumpTimer < 16){
          this.jumpTimer++;
          this.directionY =- this.jumpForce - (this.jumpTimer / 50)
      }
@@ -105,6 +111,48 @@ class Player {
     }
 }
 
+class Obstacle {
+    constructor(x,y,width,height,color){
+       this.x = x;
+       this.y = y;
+       this.width = width;
+       this.height = height;
+       this.color = color;
+
+       this.directionX = -gameSpeed;
+    }
+
+    Update () {
+        this.x += this.directionX;
+        this.Draw();
+        this.directionX = -gameSpeed;
+    }
+
+    Draw () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.closePath();
+    }
+}
+//Game Functions
+//Spawn Obstacles
+function SpawnObstacle () {
+    let size = RandomIntRange(20, 70); // random size of the spawned obstacle
+    // console.log(size);
+    let type = RandomIntRange(0,1);
+    let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, "#2484E4" );
+
+    if(type == 1) {
+        obstacle.y -= player.originalHeight - 10;
+    }
+    obstacles.push(obstacle);
+}
+
+
+function RandomIntRange(min, max) {
+    return Math.round(Math.random() * (max - min) + min); //Return a random number in the size variable, between (20, 70)
+}
 // upon Start draw the player to the canvas
 function Start () {
     canvas.width = window.innerWidth;
@@ -123,9 +171,31 @@ function Start () {
 }
 
 //Update animation frame & clear canvas upon change
+let initialSpawnTimer = 200;
+let spawnTimer = initialSpawnTimer;
+
 function Update () {
+
     requestAnimationFrame(Update);
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    spawnTimer--;
+    if(spawnTimer <= 0){
+        SpawnObstacle();
+        console.log(obstacles)
+        spawnTimer = initialSpawnTimer - gameSpeed * 8;
+    
+      if(spawnTimer < 60) {
+          spawnTimer = 60;
+      }
+    }
+
+    //Spawn obstacles
+    for(let i = 0; i < obstacles.length; i++){
+        let o = obstacles[i]
+       
+        o.Update();
+    }
 
     player.Animate();
     
